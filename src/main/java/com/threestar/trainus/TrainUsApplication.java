075b7bc0ad48aac3b74e1fd06e2ebf0be658a7a9
Ji-minhyeok <gmh8257@gmail.com>
2025-07-08 10:23:48 +0900

    fitst commit

---
+++ src/main/java/com/threestar/trainus/TrainUsApplication.java
@@ -0,0 +1,13 @@
+package com.threestar.trainus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainUsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainUsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/threestar/trainus/TrainUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threestar.trainus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainUsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
