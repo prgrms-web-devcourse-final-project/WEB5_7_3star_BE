7a86da9cc54d658f1b00a1d3d4072b3592c1c20c
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2025-07-11 16:11:42 +0900

    feat : 레슨강사용 - 로그인, 프로필 데이터 연동
    * refactor: securityConfig에 lesson추가
    
    * refactor:레슨참가자신청목록조회시 프로필과연동
    
    * 레슨강사용:로그인,프로필데이터연동
    
    * fix:레슨강사용 컨트롤러 변수명통일

--- src/main/java/com/threestar/trainus/domain/lesson/admin/dto/LessonApplicationResponseDto.java
+++ src/main/java/com/threestar/trainus/domain/lesson/admin/dto/LessonApplicationResponseDto.java
@@ -2,6 +2,8 @@ package com.threestar.trainus.domain.lesson.admin.dto;
 
 import java.time.LocalDateTime;
 
+import com.threestar.trainus.domain.profile.dto.ProfileResponseDto;
+
 import lombok.Builder;
 
 /**
@@ -10,7 +12,7 @@ import lombok.Builder;
 @Builder
 public record LessonApplicationResponseDto(
 	Long lessonApplicationId,
-	UserSimpleDto user, //TODO: profile 도메인 완성 후 ProfileDto로 변경 예정
+	ProfileResponseDto user,
 	String status,
 	LocalDateTime appliedAt
 ) {
