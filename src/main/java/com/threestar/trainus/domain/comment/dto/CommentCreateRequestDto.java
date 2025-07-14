1896fd385b81b809051ef4bc408f4bd744732706
나상연 <reddynor@naver.com>
2025-07-14 14:17:04 +0900

    feat: repository 및 request dto 제약조건 수정

--- src/main/java/com/threestar/trainus/domain/comment/dto/CommentCreateRequestDto.java
+++ src/main/java/com/threestar/trainus/domain/comment/dto/CommentCreateRequestDto.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class CommentCreateRequestDto {
 	@NotBlank(message = "댓글 내용은 필수입니다")
-	@Size(max = 50, message = "댓글은 50자 이내여야 합니다.")
+	@Size(max = 255, message = "댓글은 255자 이내여야 합니다.")
 	private String content;
 	private Long parentCommentId;
 }
