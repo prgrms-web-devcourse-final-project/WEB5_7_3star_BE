e6877b50f209cf423a5f5efcc74f168eacf2b1ac
limchangin <dlackddls5@naver.com>
2025-07-14 09:51:45 +0900

    refacttor: codeStyle 적용

--- src/main/java/com/threestar/trainus/domain/user/controller/UserController.java
+++ src/main/java/com/threestar/trainus/domain/user/controller/UserController.java
@@ -1,21 +1,15 @@
 package com.threestar.trainus.domain.user.controller;
 
-import java.security.Principal;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.threestar.trainus.domain.user.dto.EmailVerificationDto;
 import com.threestar.trainus.domain.user.dto.EmailSendRequestDto;
 import com.threestar.trainus.domain.user.dto.EmailSendResponseDto;
+import com.threestar.trainus.domain.user.dto.EmailVerificationDto;
 import com.threestar.trainus.domain.user.dto.LoginRequestDto;
 import com.threestar.trainus.domain.user.dto.LoginResponseDto;
 import com.threestar.trainus.domain.user.dto.NicknameCheckRequestDto;
