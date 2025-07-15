1d4f12f2d84bb0bb48574590fa3b2c2df829877d
limchangin <dlackddls5@naver.com>
2025-07-15 17:28:02 +0900

    refactor: checkStyle

--- src/main/java/com/threestar/trainus/domain/ranking/repository/RankingRepository.java
+++ src/main/java/com/threestar/trainus/domain/ranking/repository/RankingRepository.java
@@ -4,28 +4,27 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.threestar.trainus.domain.lesson.admin.entity.Category;
 import com.threestar.trainus.domain.metadata.entity.ProfileMetadata;
 import com.threestar.trainus.domain.ranking.dto.RankingData;
 
-import io.lettuce.core.dynamic.annotation.Param;
-
 public interface RankingRepository extends JpaRepository<ProfileMetadata, Long> {
 
 	@Query("""
 		SELECT pm.user.id as userId,
-		       pm.user.nickname as userNickname, 
-		       pm.reviewCount as reviewCount,
-		       pm.rating as rating,
-		       p.profileImage as profileImage
+			pm.user.nickname as userNickname,
+			pm.reviewCount as reviewCount,
+			pm.rating as rating,
+			p.profileImage as profileImage
 		FROM ProfileMetadata pm
 		JOIN pm.user u
 		LEFT JOIN Profile p ON p.user = u
-		WHERE pm.reviewCount >= 10
+		WHERE pm.reviewCount >= 20
 		ORDER BY (
-		    (pm.rating / 5.0) * 0.5 + 
-		    (LEAST(pm.reviewCount, 100) / 100.0) * 0.5
+			(pm.rating / 5.0) * 0.5 +
+			(LEAST(pm.reviewCount, 100) / 100.0) * 0.5
 		) DESC
 		LIMIT 10
 		""")
@@ -33,24 +32,23 @@ public interface RankingRepository extends JpaRepository<ProfileMetadata, Long> 
 
 	@Query("""
 		SELECT r.reviewee.id as userId,
-		           r.reviewee.nickname as userNickname,
-		           CAST(COUNT(r.reviewId) AS INTEGER) as reviewCount,
-		           CAST(AVG(r.rating) AS FLOAT) as rating,
-		           p.profileImage as profileImage
-		    FROM Review r
-		    JOIN r.lesson l
-		    JOIN r.reviewee u
-		    LEFT JOIN Profile p ON p.user = u
-		    WHERE l.category = :category
-		    AND r.deletedAt IS NULL
-		    GROUP BY r.reviewee.id, r.reviewee.nickname, p.profileImage
-		    HAVING COUNT(r.reviewId) >= 10
-		    ORDER BY (
-		        (AVG(r.rating) / 5.0) * 0.5 +
-		        (LEAST(COUNT(r.reviewId), 100) / 100.0) * 0.5
-		    ) DESC
-		    LIMIT 10
+			r.reviewee.nickname as userNickname,
+			CAST(COUNT(r.reviewId) AS INTEGER) as reviewCount,
+			AST(AVG(r.rating) AS FLOAT) as rating,
+			p.profileImage as profileImage
+		FROM Review r
+		JOIN r.lesson l
+		JOIN r.reviewee u
+		LEFT JOIN Profile p ON p.user = u
+		WHERE l.category = :category
+		AND r.deletedAt IS NULL
+		GROUP BY r.reviewee.id, r.reviewee.nickname, p.profileImage
+		HAVING COUNT(r.reviewId) >= 20
+		ORDER BY (
+			(AVG(r.rating) / 5.0) * 0.5 +
+			(LEAST(COUNT(r.reviewId), 100) / 100.0) * 0.5
+		) DESC
+		LIMIT 10
 		""")
 	List<RankingData> findTopRankingsByCategory(@Param("category") Category category);
-
 }
