c352a86caafdeaabed1b498a5be57ce771403858
Taeho KIM <laferrari963@naver.com>
2025-07-15 15:04:12 +0900

    feat: swagger tag추가, 메서드재사용

--- src/main/java/com/threestar/trainus/domain/coupon/controller/CouponController.java
+++ src/main/java/com/threestar/trainus/domain/coupon/controller/CouponController.java
@@ -14,13 +14,14 @@ import com.threestar.trainus.domain.coupon.dto.CreateUserCouponResponseDto;
 import com.threestar.trainus.domain.coupon.dto.UserCouponPageResponseDto;
 import com.threestar.trainus.domain.coupon.entity.CouponStatus;
 import com.threestar.trainus.domain.coupon.service.CouponService;
-import com.threestar.trainus.global.exception.domain.ErrorCode;
-import com.threestar.trainus.global.exception.handler.BusinessException;
+import com.threestar.trainus.global.annotation.LoginUser;
 import com.threestar.trainus.global.unit.BaseResponse;
 
-import jakarta.servlet.http.HttpSession;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "쿠폰 API", description = "쿠폰 발급/조회 관련 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/coupons")
@@ -29,37 +30,30 @@ public class CouponController {
 	private final CouponService couponService;
 
 	@PostMapping("/{couponId}")
-	public ResponseEntity<BaseResponse<CreateUserCouponResponseDto>> createUserCoupon(@PathVariable Long couponId,
-		HttpSession session
+	@Operation(summary = "쿠폰 발급 API", description = "couponId에 맞는 쿠폰을 유저에게 발급하는 API입니다.")
+	public ResponseEntity<BaseResponse<CreateUserCouponResponseDto>> createUserCoupon(
+		@PathVariable Long couponId,
+		@LoginUser Long userId
 	) {
-		Long userId = (Long)session.getAttribute("LOGIN_USER");
-		if (userId == null) {
-			throw new BusinessException(ErrorCode.AUTHENTICATION_REQUIRED);
-		}
 		CreateUserCouponResponseDto dto = couponService.createUserCoupon(userId, couponId);
 
 		return BaseResponse.ok("쿠폰 발급 완료", dto, HttpStatus.CREATED);
 	}
 
 	@GetMapping("/my-coupons")
+	@Operation(summary = "내 쿠폰 목록 조회 API", description = "내가 보유한 쿠폰 목록을 조회하는 API입니다.")
 	public ResponseEntity<BaseResponse<UserCouponPageResponseDto>> getUserCoupons(
-		@RequestParam(required = false) CouponStatus status, HttpSession session
+		@RequestParam(required = false) CouponStatus status,
+		@LoginUser Long userId
 	) {
-		Long userId = (Long)session.getAttribute("LOGIN_USER");
-		if (userId == null) {
-			throw new BusinessException(ErrorCode.AUTHENTICATION_REQUIRED);
-		}
 		UserCouponPageResponseDto dto = couponService.getUserCoupons(userId, status);
 
 		return BaseResponse.ok("사용자 보유 쿠폰 조회 성공", dto, HttpStatus.OK);
 	}
 
 	@GetMapping
-	public ResponseEntity<BaseResponse<CouponPageResponseDto>> getCoupons(HttpSession session) {
-		Long userId = (Long)session.getAttribute("LOGIN_USER");
-		if (userId == null) {
-			throw new BusinessException(ErrorCode.AUTHENTICATION_REQUIRED);
-		}
+	@Operation(summary = "발급 가능 쿠폰 목록 조회 API", description = "발급 가능한 쿠폰 목록을 조회하는 API입니다.")
+	public ResponseEntity<BaseResponse<CouponPageResponseDto>> getCoupons(@LoginUser Long userId) {
 		CouponPageResponseDto dto = couponService.getCoupons(userId);
 
 		return BaseResponse.ok("발급가능한 쿠폰 조회 성공", dto, HttpStatus.OK);
