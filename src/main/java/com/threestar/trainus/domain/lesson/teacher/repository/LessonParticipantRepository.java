4a5422e226b3bcd54a96ece5e2025e3a2149bde9
Ji-minhyeok <gmh8257@gmail.com>
2025-07-18 17:18:44 +0900

    test: 동시요청 락 미적용 최대참가자 초과 테스트 작성

--- src/main/java/com/threestar/trainus/domain/lesson/teacher/repository/LessonParticipantRepository.java
+++ src/main/java/com/threestar/trainus/domain/lesson/teacher/repository/LessonParticipantRepository.java
@@ -1,10 +1,21 @@
 package com.threestar.trainus.domain.lesson.teacher.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.threestar.trainus.domain.lesson.teacher.entity.LessonParticipant;
 
+import jakarta.transaction.Transactional;
+
 public interface LessonParticipantRepository extends CrudRepository<LessonParticipant, Long> {
 
 	boolean existsByLessonIdAndUserId(Long lessonId, Long userId);
+
+	long countByLessonId(Long lessonId);
+
+	@Modifying
+	@Transactional
+	@Query("DELETE FROM LessonParticipant lp WHERE lp.lesson.id = :lessonId")
+	void deleteByLessonId(Long lessonId);
 }
