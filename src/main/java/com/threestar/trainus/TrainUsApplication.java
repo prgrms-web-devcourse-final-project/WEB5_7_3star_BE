6a2d87a5d167214a6c15222dd9dfdfd9af059d03
나상연 <reddynor@naver.com>
2025-07-29 12:22:54 +0900

    feat: 타임존 관련 추가

--- src/main/java/com/threestar/trainus/TrainUsApplication.java
+++ src/main/java/com/threestar/trainus/TrainUsApplication.java
@@ -1,7 +1,11 @@
 package com.threestar.trainus;
 
+import java.util.TimeZone;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TrainUsApplication {
@@ -10,4 +14,10 @@ public class TrainUsApplication {
 		SpringApplication.run(TrainUsApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner init() {
+		return args -> {
+			TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		};
+	}
 }
