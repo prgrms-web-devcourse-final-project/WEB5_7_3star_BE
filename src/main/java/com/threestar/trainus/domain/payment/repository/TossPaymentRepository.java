4f953768ab71393bc3f801b66455bcb6d92c67cd
나상연 <reddynor@naver.com>
2025-08-30 12:38:43 +0900

    refactor: 쿼리 관련 리팩토링

--- src/main/java/com/threestar/trainus/domain/payment/repository/TossPaymentRepository.java
+++ src/main/java/com/threestar/trainus/domain/payment/repository/TossPaymentRepository.java
@@ -1,12 +1,21 @@
 package com.threestar.trainus.domain.payment.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.threestar.trainus.domain.payment.entity.TossPayment;
 
 public interface TossPaymentRepository extends JpaRepository<TossPayment, Long> {
 
 	Optional<TossPayment> findByOrderId(String orderId);
+
+	@Query("""
+		SELECT t FROM TossPayment t
+		WHERE t.orderId IN :orderIds
+	""")
+	List<TossPayment> findAllByOrderIds(@Param("orderIds") List<String> orderIds);
 }
