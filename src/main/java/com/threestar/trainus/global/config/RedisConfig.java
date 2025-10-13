be3d8320bf9d5845dc7f5f42c2e7272304dd0224
Taeho KIM <laferrari963@naver.com>
2025-10-13 16:18:46 +0900

    feaet: redis분산락 구현

--- src/main/java/com/threestar/trainus/global/config/RedisConfig.java
+++ src/main/java/com/threestar/trainus/global/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.threestar.trainus.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -9,8 +8,6 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-
 
 @Configuration
 public class RedisConfig {
