c9b6e67ab549d2a1496a23bf7cc58a84de4a67c7
Ji-minhyeok <gmh8257@gmail.com>
2025-12-16 19:56:57 +0900

    feat: 로컬 환경 테스트를 위한 RedisConfig 분기점 추가

--- src/main/java/com/threestar/trainus/global/config/RedisConfig.java
+++ src/main/java/com/threestar/trainus/global/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.threestar.trainus.global.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,22 +23,29 @@ public class RedisConfig {
 	@Value("${spring.data.redis.port}")
 	private int port;
 
-	@Value("${spring.data.redis.username}")
+	@Value("${spring.data.redis.username:}")
 	private String username;
 
-	@Value("${spring.data.redis.password}")
+	@Value("${spring.data.redis.password:}")
 	private String password;
 
-	private static final String REDISSON_HOST_PREFIX = "rediss://";
+	@Value("${spring.data.redis.key.prefix}")
+	private String prefix;
 
 	@Bean
 	public RedissonClient redissonClient() {
 		Config config = new Config();
+		SingleServerConfig singleServerConfig = config.useSingleServer();
 
-		config.useSingleServer()
-			.setAddress(REDISSON_HOST_PREFIX + host + ":" + port)
-			.setUsername(username)
-			.setPassword(password);
+		singleServerConfig.setAddress(prefix + host + ":" + port);
+
+		// username과 password가 실제 값이 있을 때만 설정 (local에선 null로 테스트 요망)
+		if (username != null && !username.isEmpty()) {
+			singleServerConfig.setUsername(username);
+		}
+		if (password != null && !password.isEmpty()) {
+			singleServerConfig.setPassword(password);
+		}
 		return Redisson.create(config);
 	}
 
