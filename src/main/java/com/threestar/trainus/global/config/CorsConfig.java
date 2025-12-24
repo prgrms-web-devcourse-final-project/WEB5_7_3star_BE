2ae99585f23bcf8910775d7817a4ca803602e9b1
Ji-minhyeok <gmh8257@gmail.com>
2025-12-24 16:48:13 +0900

    feat: 로드밸런서 DNS CorsConfig 정책 추가

--- src/main/java/com/threestar/trainus/global/config/CorsConfig.java
+++ src/main/java/com/threestar/trainus/global/config/CorsConfig.java
@@ -12,9 +12,11 @@ public class CorsConfig implements WebMvcConfigurer {
 			.allowedOrigins("http://localhost:3000",
 				"http://localhost:8080",
 				"http://localhost:8031",
+				"http://localhost:5173",
 				"http://15.165.184.145:8080",
 				"http://15.165.184.145:3000",
-				"http://15.165.184.145:8031")
+				"http://15.165.184.145:8031",
+				"http://trainus-alb-1227831319.ap-northeast-2.elb.amazonaws.com")
 			.allowCredentials(true) // 쿠키 허용
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowedHeaders("*");
