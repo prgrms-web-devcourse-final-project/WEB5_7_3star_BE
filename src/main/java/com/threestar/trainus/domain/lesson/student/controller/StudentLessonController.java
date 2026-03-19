32b6054ef9b9268b2faffedc75dcb6fc61830d51
Ji-minhyeok <gmh8257@gmail.com>
2026-03-19 17:42:53 +0900

    feat: Redis Stream 부하 테스트 환경 구축 및 비동기 신청 성능 최적화

--- src/main/java/com/threestar/trainus/domain/lesson/student/controller/StudentLessonController.java
+++ src/main/java/com/threestar/trainus/domain/lesson/student/controller/StudentLessonController.java
@@ -1,6 +1,5 @@
 package com.threestar.trainus.domain.lesson.student.controller;
 
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
