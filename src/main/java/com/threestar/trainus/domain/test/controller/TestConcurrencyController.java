0b9e6b9646ac6cbf152279a2d4068a3b24392d80
JiMinhyeok <87597610+Ji-minhyeok@users.noreply.github.com>
2026-03-22 00:56:28 +0900

    feat: JWT 기반 고성능 부하 테스트 환경 구축 및 최적화 (#204)
    * refactor: 세션 기반 사용자 인증을 JWT 무상태 인증으로 전면 교체
    
    * chore: 주석 스타일 통일 및 가독성 개선
    
    * feat: JWT 기반 고성능 부하 테스트 환경 구축 및 최적화
    
    * chore: 누락된 yml 주석 수정

--- src/main/java/com/threestar/trainus/domain/test/controller/TestConcurrencyController.java
+++ src/main/java/com/threestar/trainus/domain/test/controller/TestConcurrencyController.java
@@ -1,10 +1,11 @@
 package com.threestar.trainus.domain.test.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,8 @@ import com.threestar.trainus.domain.lesson.student.dto.LessonApplicationResponse
 import com.threestar.trainus.domain.lesson.student.service.StudentLessonFacade;
 import com.threestar.trainus.domain.lesson.student.service.StudentLessonService;
 import com.threestar.trainus.domain.lesson.teacher.service.AdminLessonService;
-import com.threestar.trainus.domain.test.dto.TestRequestDto;
 import com.threestar.trainus.domain.test.service.TestUserService;
-import com.threestar.trainus.domain.user.entity.User;
+import com.threestar.trainus.global.annotation.LoginUser;
 import com.threestar.trainus.global.unit.BaseResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -43,56 +43,54 @@ public class TestConcurrencyController {
 	private final CouponIssueFacade couponIssueFacade;
 
 	@PostMapping("/users/create")
-	@Operation(summary = "테스트 유저 대량 생성", description = "지정된 수만큼 테스트 유저를 미리 생성합니다.")
+	@Operation(summary = "테스트 유저 생성", description = "지정된 수만큼 테스트 유저를 미리 생성합니다.")
 	public ResponseEntity<String> createUsers(@RequestParam int count) {
-		testUserService.createUsers(count); // Todo setup 메서드로 전환
+		testUserService.createUsers(count);
 		return ResponseEntity.ok(count + "명의 테스트 유저 생성 완료");
+	}
+
+	@PostMapping("/users/tokens/csv")
+	@Operation(summary = "테스트 유저 토큰 CSV 생성", description = "기존 테스트 유저들의 ID와 JWT 토큰이 담긴 CSV 파일을 생성하여 반환합니다.")
+	public ResponseEntity<byte[]> generateTokenCsv() {
+		String csvContent = testUserService.generateTokenCsvForTestUsers();
+		byte[] csvBytes = csvContent.getBytes();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.parseMediaType("text/csv"));
+		headers.setContentDispositionFormData("attachment", "test_user_tokens.csv");
+
+		return new ResponseEntity<>(csvBytes, headers, HttpStatus.OK);
 	}
 
 	// 쿠폰 동시성 테스트
 	@PostMapping("/coupons/{couponId}/no-lock")
 	@Operation(summary = "쿠폰 발급 동시성 테스트 (락 미적용)", description = "쿠폰을 발급받는 테스트 API (락 미적용)")
-	public ResponseEntity<BaseResponse<CreateUserCouponResponseDto>> issueCouponNoLock(
-		@PathVariable Long couponId,
-		@RequestBody TestRequestDto testRequestDto
-	) {
-		User user = testUserService.findOrCreateUser(testRequestDto.getUserId());
-		CreateUserCouponResponseDto responseDto = couponService.createUserCouponWithoutLock(user.getId(), couponId);
+	public ResponseEntity<BaseResponse<CreateUserCouponResponseDto>> issueCouponNoLock(@PathVariable Long couponId,
+		@LoginUser Long userId) {
+		CreateUserCouponResponseDto responseDto = couponService.createUserCouponWithoutLock(userId, couponId);
 		return BaseResponse.ok("쿠폰 발급 완료 (락 미적용)", responseDto, HttpStatus.CREATED);
 	}
 
 	@PostMapping("/coupons/{couponId}/pessimistic-lock")
 	@Operation(summary = "쿠폰 발급 동시성 테스트 (비관적 락)", description = "쿠폰을 발급받는 테스트 API (비관적 락)")
 	public ResponseEntity<BaseResponse<CreateUserCouponResponseDto>> issueCouponPessimisticLock(
-		@PathVariable Long couponId,
-		@RequestBody TestRequestDto testRequestDto
-	) {
-		User user = testUserService.findOrCreateUser(testRequestDto.getUserId());
-		CreateUserCouponResponseDto responseDto = couponService.createUserCouponWithPessimisticLock(user.getId(),
-			couponId);
+		@PathVariable Long couponId, @LoginUser Long userId) {
+		CreateUserCouponResponseDto responseDto = couponService.createUserCouponWithPessimisticLock(userId, couponId);
 		return BaseResponse.ok("쿠폰 발급 완료 (비관적 락)", responseDto, HttpStatus.CREATED);
 	}
 
 	@PostMapping("/coupons/{couponId}/distributed-lock")
 	@Operation(summary = "쿠폰 발급 동시성 테스트 (분산 락)", description = "쿠폰을 발급받는 테스트 API (분산 락)")
 	public ResponseEntity<BaseResponse<CreateUserCouponResponseDto>> issueCouponDistributedLock(
-		@PathVariable Long couponId,
-		@RequestBody TestRequestDto testRequestDto
-	) {
-		User user = testUserService.findOrCreateUser(testRequestDto.getUserId());
-		CreateUserCouponResponseDto responseDto = couponIssueFacade.issueCoupon(user.getId(),
-			couponId);
+		@PathVariable Long couponId, @LoginUser Long userId) {
+		CreateUserCouponResponseDto responseDto = couponIssueFacade.issueCoupon(userId, couponId);
 		return BaseResponse.ok("쿠폰 발급 완료 (분산 락)", responseDto, HttpStatus.CREATED);
 	}
 
 	@PostMapping("/coupons/{couponId}/redis-stream")
 	@Operation(summary = "쿠폰 발급 동시성 테스트 (메시지 큐)", description = "쿠폰을 발급받는 테스트 API (메시지 큐)")
-	public ResponseEntity<?> issueCouponRedisStream(
-		@PathVariable Long couponId,
-		@RequestBody TestRequestDto testRequestDto
-	) {
-		User user = testUserService.findOrCreateUser2(testRequestDto.getUserId());
-		boolean ok = couponIssueProducer.send(couponId, user.getId());
+	public ResponseEntity<?> issueCouponRedisStream(@PathVariable Long couponId, @LoginUser Long userId) {
+		boolean ok = couponIssueProducer.send(couponId, userId);
 		if (!ok) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).body("sold out");
 		}
@@ -102,46 +100,32 @@ public class TestConcurrencyController {
 	// 레슨 신청 동시성 테스트
 	@PostMapping("/lessons/{lessonId}/application/no-lock")
 	@Operation(summary = "레슨 신청 동시성 테스트 (락 미적용)", description = "레슨을 신청하는 테스트 API (락 미적용)")
-	public ResponseEntity<BaseResponse<LessonApplicationResponseDto>> applyToLessonNoLock(
-		@PathVariable Long lessonId,
-		@RequestBody TestRequestDto testRequestDto
-	) {
-		User user = testUserService.findOrCreateUser(testRequestDto.getUserId());
-		LessonApplicationResponseDto response = studentLessonService.applyToLessonWithoutLock(lessonId, user.getId());
+	public ResponseEntity<BaseResponse<LessonApplicationResponseDto>> applyToLessonNoLock(@PathVariable Long lessonId,
+		@LoginUser Long userId) {
+		LessonApplicationResponseDto response = studentLessonService.applyToLessonWithoutLock(lessonId, userId);
 		return BaseResponse.ok("레슨 신청 완료 (락 미적용)", response, HttpStatus.OK);
 	}
 
 	@PostMapping("/lessons/{lessonId}/application/pessimistic-lock")
 	@Operation(summary = "레슨 신청 동시성 테스트 (비관적 락)", description = "레슨을 신청하는 테스트 API (비관적 락)")
 	public ResponseEntity<BaseResponse<LessonApplicationResponseDto>> applyToLessonPessimisticLock(
-		@PathVariable Long lessonId,
-		@RequestBody TestRequestDto testRequestDto
-	) {
-		User user = testUserService.findOrCreateUser(testRequestDto.getUserId());
-		LessonApplicationResponseDto response = studentLessonService.applyToLessonWithPessimisticLock(lessonId,
-			user.getId());
+		@PathVariable Long lessonId, @LoginUser Long userId) {
+		LessonApplicationResponseDto response = studentLessonService.applyToLessonWithPessimisticLock(lessonId, userId);
 		return BaseResponse.ok("레슨 신청 완료 (비관적 락)", response, HttpStatus.OK);
 	}
 
 	@PostMapping("/lessons/{lessonId}/application/distributed-lock")
 	@Operation(summary = "레슨 신청 동시성 테스트 (분산 락)", description = "레슨을 신청하는 테스트 API (분산 락)")
 	public ResponseEntity<BaseResponse<LessonApplicationResponseDto>> applyToLessonDistributedLock(
-		@PathVariable Long lessonId,
-		@RequestBody TestRequestDto testRequestDto
-	) {
-		User user = testUserService.findOrCreateUser(testRequestDto.getUserId());
-		LessonApplicationResponseDto response = studentLessonFacade.applyToLessonWithDistributedLock(lessonId,
-			user.getId());
+		@PathVariable Long lessonId, @LoginUser Long userId) {
+		LessonApplicationResponseDto response = studentLessonFacade.applyToLessonWithDistributedLock(lessonId, userId);
 		return BaseResponse.ok("레슨 신청 완료 (분산 락)", response, HttpStatus.OK);
 	}
 
 	@PostMapping("/lessons/{lessonId}/application/redis-stream")
 	@Operation(summary = "레슨 신청 동시성 테스트 (메시지 큐)", description = "레슨을 신청하는 테스트 API (메시지 큐)")
-	public ResponseEntity<?> applyToLessonRedisStream(
-		@PathVariable Long lessonId,
-		@RequestBody TestRequestDto testRequestDto
-	) {
-		String requestId = lessonApplyProducer.send(lessonId, testRequestDto.getUserId());
+	public ResponseEntity<?> applyToLessonRedisStream(@PathVariable Long lessonId, @LoginUser Long userId) {
+		String requestId = lessonApplyProducer.send(lessonId, userId);
 		if (requestId == null) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).body("sold out");
 		}
